90f689eda2a12003e5dea0f2de3e51d0f8fba845
unknown <dushimimanafabricerwanda@gmail.com>
2025-11-18 15:44:56 +0100

    Added route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,26 @@
+import User from "../models/userModel.js";
+
 export const deleteMe = async function (req, res, next) {
   try {
+    //? get password and check if there is user associated to it
+    const user = await User.findOne({ password: req.body.password });
   } catch (err) {}
 };
 
 export const getMe = async function (req, res, next) {
   try {
-  } catch (err) {}
+    const user = await User.findOne(req.user._id);
+
+    res.status(200).json({
+      status: "success",
+      message: "Data fetched successfully!",
+      data: { user },
+    });
+  } catch (err) {
+    req
+      .status(500)
+      .json({ status: "fail", message: err.message, errors: { err } });
+  }
 };
 
 export const updateMe = async function (req, res, next) {
